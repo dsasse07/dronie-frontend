5b18e79cdc93eefeaabe78de50e2a6f5f09529cb
Daniel Sasse <dsasse07@gmail.com>
2021-03-18 20:39:33 -0400

    Dropzone files register

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,16 +1,48 @@
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { useSelector } from 'react-redux'
 import { IonInput, IonLabel, IonItem, IonCard, IonCardContent, IonThumbnail } from "@ionic/react"
 import styled from 'styled-components'
 import FileDrop from '../components/FileDrop'
+import {useDropzone} from 'react-dropzone';
 
 function NewPostForm() {
   const user = useSelector(state => state.user)
   
-  const { register, handleSubmit, watch, errors } = useForm();  
+  const { register, handleSubmit, watch, errors, control } = useForm();  
   const onSubmit = data => console.log(data);
 
   console.log(watch("images")); // watch input value by passing the name of it
+
+
+
+function Basic(props) {
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+    accept: 'image/*',
+    maxFiles: 5,
+    onDrop: props.onChange,
+    multiple: true
+  });
+  
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <section className="container">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  );
+}
+
 
 
   return (
@@ -18,9 +50,25 @@ function NewPostForm() {
       <IonCardContent>
 
           <form onSubmit={handleSubmit(onSubmit)}>
-            {/* <IonItem> */}
-              <FileDrop />
-            {/* </IonItem> */}
+              
+          <Controller
+            control={control}
+            name="images"
+            render={(
+              { onChange, onBlur, value, name, ref },
+              { invalid, isTouched, isDirty }
+            ) => (
+              <Basic
+                onBlur={onBlur}
+                onChange={(e) => onChange(e)}
+                checked={value}
+                inputRef={ref}
+              />
+            )}
+          />
+              
+              
+              {/* <Basic /> */}
             
 
 
